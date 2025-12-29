2ce890ed54418a325f9d43de4dfb23767488d81f
Kajta <kajtakamil1@gmail.com>
2025-12-29 10:51:33 +0100

    fix: xcom scraper

--- src/price/scrapers/xkom.scraper.ts
+++ src/price/scrapers/xkom.scraper.ts
@@ -7,48 +7,39 @@ export class XkomScraper implements PriceScraper {
   }
   async scrape(url: string): Promise<number | null> {
     const browser = await chromium.launch({ headless: true });
-
     const context = await browser.newContext({
       userAgent:
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36',
-      locale: 'en-US',
+      locale: 'pl-PL',
     });
 
     const page = await context.newPage();
 
-    await page
-      .goto(url, {
-        waitUntil: 'networkidle',
+    try {
+      await page.goto(url, {
+        waitUntil: 'domcontentloaded',
         timeout: 45000,
-      })
-      .catch(async (e) => {
-        console.error('Błąd podczas ładowania strony:', e);
-        await browser.close();
-        return null;
       });
 
-    await page
-      .waitForSelector('meta[property="product:price:amount"]', {
-        timeout: 10000,
-      })
-      .catch(async () => {
-        console.warn('Nie znaleziono selektora ceny na stronie X-Kom.');
-        await browser.close();
+      const price = await page
+        .locator('meta[property="product:price:amount"]')
+        .getAttribute('content');
+
+      if (!price) {
         return null;
-      });
+      }
 
-    const price = await page
-      .locator('meta[property="product:price:amount"]')
-      .getAttribute('content');
-    await browser.close();
+      const numericPrice = parseFloat(price.replace(',', '.'));
+      if (isNaN(numericPrice)) {
+        return null;
+      }
 
-    if (!price) {
-      throw new Error('Pod tym adresem nie ma ceny');
+      return numericPrice;
+    } catch (e) {
+      console.warn('Scrape nieudany:', e);
+      return null;
+    } finally {
+      await browser.close();
     }
-    const numericPrice = parseFloat(price.replace(',', '.'));
-    if (isNaN(numericPrice)) {
-      throw new Error('Nie udało się przekonwertować ceny na liczbę.');
-    }
-    return numericPrice;
   }
 }
